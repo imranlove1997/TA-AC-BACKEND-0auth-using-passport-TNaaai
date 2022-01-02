e3846f192c72e6c1236e4e3b26ddd5433c170161
Imran Khan <imranlove1997@gmail.com>
2022-01-02 16:47:19 +0530

    BlogApp

---
+++ block-BNaaef/blogApp/models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+var userSchema = new Schema ({
+    name: { type: String },
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String}
+}, { timestamps: true })
+
+userSchema.pre('save', function(next) {
+    if(this.password && this.isModified('password')) {
+        bcrypt.hash(this.password, 10, (err, hashed) => {
+            if(err) return next(err);
+            this.password = hashed;
+            return next();
+        })
+    } else {
+        next();
+    }
+})
+
+userSchema.methods.verifyPassword = function(password, cb) {
+    bcrypt.compare(password, this.password, (err, result) => {
+        return cb(err, result);
+    })
+}
+
+module.exports = mongoose.model('User', userSchema);
